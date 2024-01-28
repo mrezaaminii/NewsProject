fd9b2e4215c821bd909e2196a8914ac38841aa88
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-01-28 17:51:25 +0330

    add soft delete to users table

--- modules/mam/User/Models/User.php
+++ modules/mam/User/Models/User.php
@@ -6,13 +6,14 @@ namespace mam\User\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
 
     protected $fillable = [
         'name',
