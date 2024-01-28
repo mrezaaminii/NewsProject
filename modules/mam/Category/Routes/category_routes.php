4d35a39dcf40097371b10522c343572c2d54c354
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-01-28 21:09:45 +0330

    make Category's routing system and introduce it inside the composer.json file

--- modules/mam/Category/Routes/category_routes.php
+++ modules/mam/Category/Routes/category_routes.php
@@ -1,4 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use mam\Category\Http\Controllers\CategoryController;
 
+Route::group(['prefix' => 'admin',['middleware' => 'auth']],function (){
+    Route::resource('/categories',CategoryController::class);
+});
