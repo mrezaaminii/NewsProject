0ef9c8b16d571450dfc999e7463d2f4a642100ed
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-01-30 17:41:49 +0330

    making role section routing system

--- modules/mam/Role/Routes/role_routes.php
+++ modules/mam/Role/Routes/role_routes.php
@@ -1,1 +1,8 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use mam\Role\Http\Controllers\RoleController;
+
+Route::group(['prefix' => 'admin','middleware'=> 'auth'],function (){
+    Route::resource('roles',RoleController::class,['except' => 'show']);
+});
