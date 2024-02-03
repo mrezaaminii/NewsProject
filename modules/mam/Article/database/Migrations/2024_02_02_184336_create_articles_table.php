160eae505c3041a5910cb8316606098c50da0aba
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-02-03 11:15:52 +0330

    making the articles table

--- modules/mam/Article/database/Migrations/2024_02_02_184336_create_articles_table.php
+++ modules/mam/Article/database/Migrations/2024_02_02_184336_create_articles_table.php
@@ -13,7 +13,19 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('time_to_read');
+            $table->string('slug')->unique();
+            $table->string('imageName');
+            $table->text('imagePath');
+            $table->string('score')->default(0);
+            $table->enum('status',\mam\Article\Models\Article::$statuses);
+            $table->enum('type',\mam\Article\Models\Article::$types)->default(\mam\Article\Models\Article::TYPE_NORMAL);
+            $table->longText('body');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
