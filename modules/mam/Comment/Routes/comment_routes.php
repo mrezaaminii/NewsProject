9ff334713d7ba5c797bb8380caeb5579b760dcf8
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-02-08 14:39:47 +0330

    set the comment module routes

--- modules/mam/Comment/Routes/comment_routes.php
+++ modules/mam/Comment/Routes/comment_routes.php
@@ -1,3 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use mam\Comment\Http\Controllers\CommentController;
 
+Route::group(['middleware' => 'auth','prefix' => 'admin'],function (){
+    Route::get('comments/all',[CommentController::class,'index'])->name('comments.index');
+    Route::put('comments/update/{id}',[CommentController::class,'update'])->name('comments.update');
+    Route::delete('comments/delete/{id}',[CommentController::class,'destroy'])->name('comments.destroy');
+});
