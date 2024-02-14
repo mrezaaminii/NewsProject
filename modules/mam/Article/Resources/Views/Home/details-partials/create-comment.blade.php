e2405cba6b49c22b0a93c19f87c003f73dea6df7
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-02-14 20:40:41 +0330

    make creating-comment view

--- modules/mam/Article/Resources/Views/Home/details-partials/create-comment.blade.php
+++ modules/mam/Article/Resources/Views/Home/details-partials/create-comment.blade.php
@@ -1,29 +1,24 @@
 <div class="comment-form">
     <h3 class="mb-30">ارسال نظرات</h3>
-    <form class="form-contact comment_form" action="#" id="commentForm">
+    <form class="form-contact comment_form" action="{{ route('comments.store') }}" id="commentForm" method="POST">
+        @csrf
+        <input type="hidden" name="commentable_id" value="{{ $article->id }}">
+        <input type="hidden" name="commentable_type" value="{{ get_class($article) }}">
         <div class="row">
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <input class="form-control" name="name" id="name" type="text" placeholder="نام">
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <input class="form-control" name="email" id="email" type="email" placeholder="ایمیل">
-                </div>
-            </div>
             <div class="col-12">
                 <div class="form-group">
-                    <input class="form-control" name="website" id="website" type="text" placeholder="سایت">
+                    <textarea class="form-control w-100 @error('body') is-invalid @enderror" name="body"
+                    id="body" cols="30" rows="9" placeholder="نظرات">
+                        {{ old('body') }}
+                    </textarea>
                 </div>
-            </div>
-            <div class="col-12">
-                <div class="form-group">
-                    <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="9" placeholder="نظرات"></textarea>
-                </div>
+                @error('body')
+                <br>
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group mt-3">
             <button type="submit" class="button button-contactForm">ارسال نظر</button>
         </div>
     </form>
