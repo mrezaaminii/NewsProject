b502088c758c0d4eee9fa2652fb3e9d1497fffa4
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-02-19 21:37:19 +0330

    prepare Advertising module folders

---
+++ modules/mam/Advertising/Providers/AdvertisingServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace mam\Advertising\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdvertisingServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        
+    }
+
+    public function boot()
+    {
+        
+    }
+}
