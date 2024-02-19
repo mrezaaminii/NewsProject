fc99c43291022cd77842ef224ec6f6e2fa1304aa
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-02-19 23:37:13 +0330

    complete the advertising request

--- modules/mam/Advertising/Http/Requests/AdvertisingRequest.php
+++ modules/mam/Advertising/Http/Requests/AdvertisingRequest.php
@@ -3,6 +3,8 @@
 namespace mam\Advertising\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use mam\Advertising\Models\Advertising;
 
 class AdvertisingRequest extends FormRequest
 {
@@ -21,8 +23,17 @@ class AdvertisingRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            //
+        $rules = [
+            'image' => 'required|image|mimes:jpg,jpeg,png|max:2048',
+            'link' => 'nullable|string|min:3',
+            'title' => 'required|string|min:3|max:255',
+            'location' => ['required','string',Rule::in(Advertising::$locations)]
         ];
+
+        if (request()->method() === 'PATCH' || request()->method() === 'PUT') {
+            $rules['image'] = 'nullable|image|mimes:jpg,jpeg,png|max:2048';
+        }
+
+        return $rules;
     }
 }
