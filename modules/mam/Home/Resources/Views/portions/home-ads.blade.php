22bfed9969d201fe5761005289e04a36c9d4c6f3
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2024-05-11 14:16:53 +0330

    fix the displaying images problem

--- modules/mam/Home/Resources/Views/portions/home-ads.blade.php
+++ modules/mam/Home/Resources/Views/portions/home-ads.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-12 text-center mb-50">
             <a href="{{ $advs_top->link }}">
-                <img class="border-radius-10 d-inline" src="{{ asset($advs_top->imagePath) }}" alt="{{ $advs_top->title }}">
+                <img class="border-radius-10 d-inline" style="width: 500px; height: 100px" src="{{ asset('storage/'.$advs_top->imagePath) }}" alt="{{ $advs_top->title }}">
             </a>
         </div>
     </div>
